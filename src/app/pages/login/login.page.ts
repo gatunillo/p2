929b7f4ee9456f5f2ad8fe02f1a27c012f0b950e
gatunillo <m.silva.pinilla@gmail.com>
2021-11-21 22:51:45 -0300

    inicio estructura

---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { 
+  FormGroup, 
+  FormControl,
+  Validators,
+  FormBuilder
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  formularioLogin: FormGroup;
+
+  constructor(public fb:FormBuilder) {
+
+    this.formularioLogin = this.fb.group({
+      'nombre': new FormControl("",Validators.required),
+      'password': new FormControl("",Validators.required)
+    });
+
+   }
+
+  ngOnInit() {
+  }
+
+  ingresar(){
+    var f = this.formularioLogin.value;
+
+    var usuario = JSON.parse(localStorage.getItem('usuario'));
+
+    
+  }
+
+}
